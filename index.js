8e9dc1f3b623ba64bf1b08a6223846e09223de28
Parvinkhuki <khuki.developer@gmail.com>
2023-08-15 19:06:55 +0600

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const btn=document.getElementById("submit");
 
 btn.addEventListener("click", function sub(){
     
- if( emailValue.value ==="khuki@gmail.com" && passwordValue.value === "shibbir"){
+ if( emailValue.value ==="khuki@gmail.com" && passwordValue.value === "shibbir ahmed"){
  window.location.href="http://127.0.0.1:5500/bank.html"
 }
 else{
